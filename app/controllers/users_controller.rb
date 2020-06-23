24155cdb3ae34a288b0d9b93ec772ac54a67440c
fujimorichihiro <fujimori.drum@gmail.com>
2020-06-23 08:20:42 +0000

    Add post-system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @articles = Article.where(user_id: current_user.id)
   end
+
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render 'edit'
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
 
+    redirect_to user_path(user)
   end
 
   def follower
@@ -19,6 +29,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).prermit(:name, :introduction, :profile_iamge)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end

930a8be249db0dc179db1d0edf56ecf1b8601663
fujimorichihiro <fujimori.drum@gmail.com>
2020-06-26 09:23:57 +0000

    Add admi-page

---
+++ app/controllers/admins/users_controller.rb
@@ -0,0 +1,20 @@
+class Admins::UsersController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :authenticate_admin!
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user =User.find(params[:id])
+    redirect_to admins_user_path(@user)
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resourse)
-    user_path(current_user)
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admins_home_top_path
+    when User
+      user_path(current_user)
+    end
   end
 
-  def after_sign_out_path_for(resourse)
-    root_path
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
   end
 
   before_action :configure_permitted_parameters, if: :devise_controller?

2c7d61450479295d8e1293ef4bec94dcc527e09b
fujimorichihiro <fujimori.drum@gmail.com>
2020-06-27 07:43:07 +0000

    update-summernote

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_26_093051) do
+ActiveRecord::Schema.define(version: 2020_06_27_070428) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -88,6 +88,12 @@ ActiveRecord::Schema.define(version: 2020_06_26_093051) do
   create_table "tags", force: :cascade do |t|
     t.integer "article_id", null: false
     t.string "tag_name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "uploads", force: :cascade do |t|
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

40a016c8fcf3705c1f1930a066346504a96a0db3
vagrant <vagrant@localhost.localdomain>
2020-07-11 06:08:34 +0000

    Fix carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'tabilogarticle'
-    config.fog_public = false
+    config.fog_public = true
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1',
       path_style: true
     }
-  else # 本番環境以外の場合はアプリケーション内にアップロード
+  else # 本番環境以外の場合
     config.storage :file
     config.enable_processing = false if Rails.env.test?
   end

e916a3e312a78f781bd4ca380abc97cc3abea45f
vagrant <vagrant@localhost.localdomain>
2020-07-21 13:12:49 +0000

    Add inquiry-system-test

---
+++ spec/system/inquiry_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'お問い合わせのテスト', type: :system do
+  describe '新規問い合わせ' do
+    before do
+      visit new_inquiry_path
+    end
+    it '未ログイン時、問い合わせに成功する' do
+      fill_in 'inquiry[email]', with: Faker::Internet.email
+      fill_in 'inquiry[title]', with: 'お問い合わせ'
+      fill_in 'inquiry[body]', with: 'お問い合わせ'
+      click_button '送信する'
+
+      expect(page).to have_content 'お問い合わせを受け付けました。'
+    end
+    it 'ログイン時、問い合わせに成功する' do
+      fill_in 'inquiry[title]', with: 'お問い合わせ'
+      fill_in 'inquiry[body]', with: 'お問い合わせ'
+      click_button '送信する'
+
+      expect(page).to have_content 'お問い合わせを受け付けました。'
+    end
+  end
+end

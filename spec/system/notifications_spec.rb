b20a8395703d3451f855068ba012bef9f85ed478
vagrant <vagrant@localhost.localdomain>
2020-08-07 14:32:27 +0000

    Edit notificatioin-test

--- spec/system/notifications_spec.rb
+++ spec/system/notifications_spec.rb
@@ -12,17 +12,49 @@ RSpec.describe '通知テスト', type: :system do
       click_button 'ログイン'
     end
     it 'いいねした時、通知が作成される' do
-      visit article_path(1)
-      expect(page).to have_content ''
+      visit article_path(id: 1)
+      click_link('favolite')
+      click_link 'ログアウト'
+      visit new_user_session_path
+      fill_in 'user[email]', with: user2.email
+      fill_in 'user[password]', with: user2.password
+      click_button 'ログイン'
+
+      find_by_id('notification-circle')
     end
     it 'ストックした時、通知が作成される' do
-      expect(page).to have_content ''
+      visit article_path(id: 1)
+      click_link('stock')
+      click_link 'ログアウト'
+      visit new_user_session_path
+      fill_in 'user[email]', with: user2.email
+      fill_in 'user[password]', with: user2.password
+      click_button 'ログイン'
+
+      find_by_id('notification-circle')
     end
     it 'コメントした時、通知が作成される' do
-      expect(page).to have_content ''
+      visit article_path(id: 1)
+      fill_in 'comment[body]', with: "コメントテスト"
+      click_button('コメント')
+      click_link 'ログアウト'
+      visit new_user_session_path
+      fill_in 'user[email]', with: user2.email
+      fill_in 'user[password]', with: user2.password
+      click_button 'ログイン'
+
+      find_by_id('notification-circle')
     end
     it 'フォローした時、通知が作成される' do
-      expect(page).to have_content ''
+      visit article_path(id: 1)
+      click_link('follow')
+      click_link 'ログアウト'
+      visit new_user_session_path
+      fill_in 'user[email]', with: user2.email
+      fill_in 'user[password]', with: user2.password
+      click_button 'ログイン'
+
+      find_by_id('notification-circle')
     end
   end
 end

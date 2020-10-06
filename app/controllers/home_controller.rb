4e82722c4c9765bf9d0c4d3ba73b81b26757a521
fujimorichihiro <fujimori.drum@gmail.com>
2020-10-06 12:18:39 +0000

    Fix layout og current-map

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
     gon.articles = Article.within_box(6.21371, latitude, longitude) - articles
     gon.stocks = Article.joins(:stocks).where(stocks: {user_id: current_user}) #ストック記事取得
     gon.latlng = [latitude, longitude] # Mapに現在地受け渡し
-    @articles = Article.within_box(6.21371, latitude, longitude)
+    @articles = Article.within_box(6.21371, latitude, longitude) #6.21371マイル = 10km
   end
 
 end

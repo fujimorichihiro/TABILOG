85ae2dca26c221b62e931a6b88d7d0b45de32770
fujimorichihiro <fujimori.drum@gmail.com>
2021-02-20 14:07:18 +0000

    Fix api

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
-    @articles = Article.all
+    @articles = Article.where(user_id: 102)
   end
 
   def show

6953c5312e43893f7c6d5f34ae811d14d3076c5d
fujimorichihiro <fujimori.drum@gmail.com>
2021-02-23 02:01:52 +0000

    Fix api controller

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SearchController < ApplicationController
+  skip_before_action :authenticate_user!
   #　場所、スポット名検索
   def by_location
     latlng = Geocoder.search(params[:search]).first.coordinates #緯度、経度を取得
